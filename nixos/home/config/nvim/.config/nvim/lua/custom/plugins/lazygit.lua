89ff1ecc24eb263cc39f3473d8020a0ec43b349a
johannes67890 <johannes@orager.dk>
2026-03-28 15:44:46 +0100

    make lazygit open i new window

--- nixos/home/config/nvim/.config/nvim/lua/custom/plugins/lazygit.lua
+++ nixos/home/config/nvim/.config/nvim/lua/custom/plugins/lazygit.lua
@@ -4,7 +4,58 @@ return {
 		"nvim-lua/plenary.nvim",
 	},
 	config = function()
-		-- <leader>g → LazyGit
-		vim.keymap.set("n", "<leader>g", "<cmd>LazyGit<CR>", { desc = "Open Lazygit" })
+		local group = vim.api.nvim_create_augroup("CustomLazyGitTerminal", { clear = true })
+
+		local function open_lazygit_tab()
+			if vim.fn.executable("lazygit") ~= 1 then
+				vim.notify("lazygit is not installed", vim.log.levels.ERROR)
+				return
+			end
+
+			local previous_tab = vim.api.nvim_get_current_tabpage()
+			vim.cmd("tabnew")
+
+			local tab = vim.api.nvim_get_current_tabpage()
+			local buf = vim.api.nvim_get_current_buf()
+
+			vim.bo[buf].buflisted = false
+			vim.bo[buf].filetype = "lazygit"
+			vim.wo.number = false
+			vim.wo.relativenumber = false
+			vim.wo.signcolumn = "no"
+			vim.wo.statuscolumn = ""
+			vim.wo.foldcolumn = "0"
+			vim.wo.cursorline = false
+			vim.wo.winbar = ""
+
+			vim.fn.termopen({ "lazygit" }, {
+				on_exit = function()
+					vim.schedule(function()
+						if vim.api.nvim_tabpage_is_valid(tab) then
+							vim.cmd.tabclose()
+						end
+
+						if vim.api.nvim_tabpage_is_valid(previous_tab) then
+							vim.api.nvim_set_current_tabpage(previous_tab)
+						end
+					end)
+				end,
+			})
+
+			vim.api.nvim_create_autocmd("TermEnter", {
+				group = group,
+				buffer = buf,
+				once = true,
+				callback = function()
+					vim.cmd("startinsert")
+				end,
+			})
+
+			vim.keymap.set("n", "q", "<cmd>tabclose<CR>", { buffer = buf, silent = true })
+			vim.keymap.set("t", "<Esc><Esc>", [[<C-\><C-n><cmd>tabclose<CR>]], { buffer = buf, silent = true })
+			vim.cmd("startinsert")
+		end
+
+		vim.keymap.set("n", "<leader>g", open_lazygit_tab, { desc = "Open Lazygit" })
 	end,
 }
